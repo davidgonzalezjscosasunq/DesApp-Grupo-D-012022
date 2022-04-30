2217404f84819362c7fa57accce348d49f08855d
emilianaAilen <emiliana.ailen@hotmail.com>
2022-04-30 20:40:30 -0300

    Voluime modal created

---
+++ src/main/java/ar/edu/unq/desapp/grupod/backenddesappapi/model/Volume.java
@@ -0,0 +1,10 @@
+package ar.edu.unq.desapp.grupod.backenddesappapi.model;
+
+import java.time.LocalDateTime;
+
+public class Volume {
+    public User user;
+    public LocalDateTime dateAndTimeRequest;
+    public Long tradedValueInUsd;
+    public Long tradedValueInPesos;
+}
--- src/main/java/ar/edu/unq/desapp/grupod/backenddesappapi/persistence/TransactionsRepository.java
+++ src/main/java/ar/edu/unq/desapp/grupod/backenddesappapi/persistence/TransactionsRepository.java
@@ -1,14 +1,18 @@
 package ar.edu.unq.desapp.grupod.backenddesappapi.persistence;
 
-import ar.edu.unq.desapp.grupod.backenddesappapi.model.Transaction;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
+import java.time.LocalDateTime;
 import java.util.List;
+
+import ar.edu.unq.desapp.grupod.backenddesappapi.model.Transaction;
+
 
 @Repository
 public interface TransactionsRepository extends CrudRepository<Transaction, Long> {
 
     List<Transaction> findAllByInterestedUserId(Long userId);
+
+    List<Transaction> findAllBetweenDates(LocalDateTime start, LocalDateTime end);
 
 }
