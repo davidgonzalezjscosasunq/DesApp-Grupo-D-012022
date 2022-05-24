6d8023d42341348a4f13f480767b3cb9bb845725
emilianaAilen <emiliana.ailen@hotmail.com>
2022-05-23 22:09:47 -0300

    get coint rate controller response fixed

--- src/main/java/ar/edu/unq/desapp/grupod/backenddesappapi/service/types/CoinRate.java
+++ src/main/java/ar/edu/unq/desapp/grupod/backenddesappapi/service/types/CoinRate.java
@@ -1,5 +1,8 @@
 package ar.edu.unq.desapp.grupod.backenddesappapi.service.types;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class CoinRate {
     private Float usdPrice;
     private Float pesosPrice;
