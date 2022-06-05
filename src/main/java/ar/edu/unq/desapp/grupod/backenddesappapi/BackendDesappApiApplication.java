0d2291b735b93c901c2cdfff6f76ab1d2399db6e
David Gonzalez <davidgonzalezjscosasunq@gmail.com>
2022-06-05 16:51:22 -0300

    Add configuration needed to deploy to Heroku

--- src/main/java/ar/edu/unq/desapp/grupod/backenddesappapi/BackendDesappApiApplication.java
+++ src/main/java/ar/edu/unq/desapp/grupod/backenddesappapi/BackendDesappApiApplication.java
@@ -2,8 +2,10 @@ package ar.edu.unq.desapp.grupod.backenddesappapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class BackendDesappApiApplication {
 
 	public static void main(String[] args) {
