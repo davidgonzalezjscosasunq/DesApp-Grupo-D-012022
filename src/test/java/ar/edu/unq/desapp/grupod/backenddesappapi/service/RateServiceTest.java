e7d5e127602fc086ec4b5041940130ecdbe832a1
David Gonzalez <davidgonzalezjscosasunq@gmail.com>
2022-06-08 01:37:58 -0300

    Get coin rate mocking request with WireMock

--- src/test/java/ar/edu/unq/desapp/grupod/backenddesappapi/service/RateServiceTest.java
+++ src/test/java/ar/edu/unq/desapp/grupod/backenddesappapi/service/RateServiceTest.java
@@ -1,5 +1,6 @@
 package ar.edu.unq.desapp.grupod.backenddesappapi.service;
 
+import ar.edu.unq.desapp.grupod.backenddesappapi.service.types.CoinRate;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import org.junit.jupiter.api.*;
@@ -15,10 +16,8 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 public class RateServiceTest extends ServiceTest {
+    private static WireMockServer binanceMockServer;
     private static WireMockServer estadisticasBCRAMockServer;
-
-    @Value("${api_estadisticasbcra_base_url}")
-    public String apiEstadisticasbcraBaseURL;
 
     @Value("${security.bcra_token}")
     public String bcraToken;
@@ -26,6 +25,7 @@ public class RateServiceTest extends ServiceTest {
     @DynamicPropertySource
     static void overrideProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
         dynamicPropertyRegistry.add("api_estadisticasbcra_base_url", estadisticasBCRAMockServer::baseUrl);
+        dynamicPropertyRegistry.add("api_binance_base_url", binanceMockServer::baseUrl);
     }
 
     @Autowired
@@ -33,12 +33,16 @@ public class RateServiceTest extends ServiceTest {
 
     @BeforeAll
     static void beforeAll() {
+        binanceMockServer = new WireMockServer(WireMockConfiguration.wireMockConfig().dynamicPort());
+        binanceMockServer.start();
+
         estadisticasBCRAMockServer = new WireMockServer(WireMockConfiguration.wireMockConfig().dynamicPort());
         estadisticasBCRAMockServer.start();
     }
 
     @AfterAll
     static void afterAll() {
+        binanceMockServer.stop();
         estadisticasBCRAMockServer.stop();
     }
 
@@ -52,5 +56,28 @@ public class RateServiceTest extends ServiceTest {
                                 .withBody("[{\"d\": \"2003-01-03\", \"v\": 999}]")));
 
         assertThat(rateService.dollarToPesoConversionRate()).isEqualTo(999);
+    }
+
+    @Test
+    public void getCoinRateSuccessfully() {
+        var symbol = "BTC";
+
+        binanceMockServer.stubFor(get("/api/v3/ticker/price?symbol=" + symbol)
+                .willReturn(
+                        aResponse()
+                                .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                                .withBody("{\"symbol\": \"BTC\", \"price\": 10}")));
+
+        estadisticasBCRAMockServer.stubFor(get("/usd")
+                .withHeader("Authorization", equalTo("Bearer " + bcraToken))
+                .willReturn(
+                        aResponse()
+                                .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                                .withBody("[{\"d\": \"2003-01-03\", \"v\": 999}]")));
+
+        var coinRate = rateService.getCoinRate(symbol);
+
+        assertThat(coinRate.usdPrice()).isEqualTo(10.f);
+        assertThat(coinRate.pesosPrice()).isEqualTo(10.f * 999.f);
     }
 }
